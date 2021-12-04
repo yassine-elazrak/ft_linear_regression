aca603d2e05258699ee3aff33dcf3aefc664e1b1
yassineazrak <yassineazrak2019@gmail.com>
2021-12-04 21:51:47 +0100

    add class LinearRegression

--- srcs/model.py
+++ srcs/model.py
@@ -1,33 +1,115 @@
 import numpy as np
+import os
+import csv
+import json
+import matplotlib.pyplot as plt
+
+class Dataset:
+    def __init__(self, path_file):
+        self.is_file(path_file)
+        self.path_file = path_file
+        self.X = []
+        self.Y = []
+
+    def is_file(self, path_file):
+        if not os.path.isfile(path_file):
+            raise ValueError("this file does not exist")
+        
+    def MinMaxScaler(self, nums):
+        return  nums - self.min / (self.max - self.min)
+    
+    def setup(self):
+        self.X = np.array(self.X)
+        self.Y = np.array(self.Y)
+        self.X = self.X.reshape((1, len(self.X)))
+        self.Y = self.Y.reshape((1, len(self.Y)))
+        self.max = np.amax(self.X)
+        self.min = np.amin(self.X)
+        self.X = self.MinMaxScaler(self.X)
+        
+        
+    
+    def read_csv(self):
+        with open(self.path_file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader)
+            for row in csv_reader:
+                self.X.append(float(row[0]))
+                self.Y.append(float(row[1]))
+            self.setup()
+
 
 
 class LinearRegression:
 
-    def __init__(self):
-        self.learning_rate = 0.001
-        self.epoch = 100000
+    def __init__(self, learning_rate=0.01, epochs=44):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.history_losses = []
 
-    def y_hat(self, X, W):
-        return np.dot(W.T, X)
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            payload = {
+                "weights": self.W.tolist(),
+                "x_max": self.X_max,
+                "x_min": self.X_min,
+            }
+        json.dump(payload, f)
+
+    
+    def ft_load(cls, filename):
+        data = json.load(filename)
+        return data
+
+    def predict(cls, X, W):
+        return np.dot(X.T, W).T
 
     def cost(self, yhat, y):
-        C = 1 / self.m * np.sum(np.power(yhat - y, 2))
+        C = 1 / 2 * self.m * np.sum(np.power(yhat - y, 2))
         return C
 
-    def gradient_descent(self, X, W, Y, yhat):
-        D = 2 / self.m * np.dot(X, (yhat - y).T)
+    def gradient_descent(self, W, X, Y, yhat):
+        D = np.dot(X, (yhat - Y).T) / self.m
         W = W - self.learning_rate * D
-    return W
+        return W
 
-    def run(self, X, Y):
+    def train(self, X, Y):
         ones = np.ones((1, X.shape[1]))
-        X = np.append(ones, X, axis=0)
+        X = np.append(X, ones, axis=0)
         self.m = X.shape[1]
         self.n = X.shape[0]
-        W = np.zeros((self.n, 1))
+        self.W = np.zeros((self.n, 1))
 
         for _ in range(self.epoch + 1):
-            yhat = self.y_hat(X, W)
-            cost = self.cost(yhat, y)
-            w = self.gradient_descent(W, X, Y, yhat)
-        return W
+            yhat = self.predict(X, self.W)
+            cost = self.cost(yhat, Y)
+            self.W = self.gradient_descent(self.W, X, Y, yhat)
+        return self.W
+class Plt:
+    
+    @classmethod
+    def plt_loss(cls, loss):
+        pass
+    
+    @classmethod
+    def plt_predict(cls, x, y):
+       
+        plt.scatter([1, 2, 3, 2, 5, 3, 7, 8, 9, 10, 11, 12],[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+        plt.plot([])
+        # plt.scatter(x,y, label="high bp low heartrate",color="r")
+        
+        plt.ylabel("y_labl")
+        plt.xlabel("x_labl")
+        plt.title("plot_data")
+        plt.legend()
+        plt.show()
+        
+
+
+if __name__ == '__main__':
+    path_file = "./data/data.csv"
+    dataset = Dataset(path_file)
+    # dataset.read_csv()
+    # lr = LinearRegression()
+    # lr.train(dataset.X, dataset.Y)
+    Plt.plt_predict(dataset.X, dataset.Y)
